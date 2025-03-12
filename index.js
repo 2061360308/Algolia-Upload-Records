0352ddef3ff5940685b610a9e76eeb03feda23aa
2061360308 <2061360308@qq.com>
2025-03-12 22:12:25 +0800

    feat:update Algolia to V5.21.0

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-
-const algoliasearch = require('algoliasearch');
-const fs = require('fs');
+const { algoliasearch } = require("algoliasearch");
+const fs = require("fs");
 
 const APPLICATION_ID = process.env.APPLICATION_ID;
 const ADMIN_API_KEY = process.env.ADMIN_API_KEY;
 const INDEX_NAME = process.env.INDEX_NAME;
 const FILE_PATH = process.env.FILE_PATH;
 
-const client = algoliasearch(APPLICATION_ID, ADMIN_API_KEY)
-const index = client.initIndex(INDEX_NAME)
+const client = algoliasearch(APPLICATION_ID, ADMIN_API_KEY);
 
 try {
-  const data = fs.readFileSync(FILE_PATH, 'utf8')
+  const data = fs.readFileSync(FILE_PATH, "utf8");
   const objects = JSON.parse(data);
-  index
-    .saveObjects(objects, { autoGenerateObjectIDIfNotExist: true })
-    .then(({ objectIDs }) => {
-      console.log(objectIDs);
+  client
+    .saveObjects({
+      indexName: INDEX_NAME,
+      objects: objects,
+      waitForTasks: true,
+    })
+    .then((response) => {
+      console.log(response);
       console.log("Save Objects End");
     });
 } catch (err) {
-  console.error(err)
+  console.error(err);
 }
